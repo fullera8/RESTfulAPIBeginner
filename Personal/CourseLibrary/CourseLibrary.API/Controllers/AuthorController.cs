f661aabb280c262887b3604317ade291035c0ac9
Alex Fuller <fulleralex47@gmail.com>
2021-02-26 06:40:44 -0500

    Create and seed DB

---
+++ Personal/CourseLibrary/CourseLibrary.API/Controllers/AuthorController.cs
@@ -0,0 +1,27 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        //private readonly ICourseLibraryRepository courseLibraryRepository;
+
+        //public AuthorController(ICourseLibraryRepository courseLibraryRepository)
+        //{
+        //    this.courseLibraryRepository = courseLibraryRepository ??
+        //        throw new ArgumentNullException(nameof(courseLibraryRepository));
+        //}
+        //public IActionResult GetAutors()
+        //{
+        //    var authorsFromRepo = this.courseLibraryRepository.GetAuthors();
+        //    return new JsonResult(authorsFromRepo);
+        //}
+    }
+}
