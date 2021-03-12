c1c1a11b42990e122bb7ede330c77acb28f74d6e
Alex Fuller <fulleralex47@gmail.com>
2021-03-12 08:21:22 -0500

    Created complex validation rules to startup. Called for 422 unprocessable for errors that do not contain correct params. This allows machine to machine communication more easily.

--- Personal/CourseLibrary/CourseLibrary.API/Startup.cs
+++ Personal/CourseLibrary/CourseLibrary.API/Startup.cs
@@ -3,6 +3,8 @@ using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,10 +25,52 @@ namespace CourseLibrary.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           services.AddControllers(setupAction => 
-           {
-               setupAction.ReturnHttpNotAcceptable = true; //Determine that accept header must be explicitly defined
-           }).AddXmlDataContractSerializerFormatters();// Allows for xml return type, JSON return by default
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true; //Determine that accept header must be explicitly defined
+           })
+            .AddXmlDataContractSerializerFormatters()// Allows for xml return type, JSON return by default
+            .ConfigureApiBehaviorOptions(setupAction =>
+            {
+                setupAction.InvalidModelStateResponseFactory = context =>
+                {
+                    //Create problem details object
+                    var problemDetailsFactory = context.HttpContext.RequestServices
+                        .GetRequiredService<ProblemDetailsFactory>();
+                    var problemDetails = problemDetailsFactory.CreateValidationProblemDetails(
+                        context.HttpContext,
+                        context.ModelState);
+
+                    // Add details not provided by default
+                    problemDetails.Detail = "See the errors field for details";
+                    problemDetails.Instance = context.HttpContext.Request.Path;
+
+                    //determine status code
+                    var actionExecutingContext = context as Microsoft.AspNetCore.Mvc.Filters.ActionExecutingContext;
+
+                    // return status code
+                    if ((context.ModelState.ErrorCount > 0) 
+                        && (actionExecutingContext?.ActionArguments.Count == context.ActionDescriptor.Parameters.Count))
+                    {
+                        problemDetails.Type = "https://courseLibrary.com/modelvalidationproblem";
+                        problemDetails.Status = StatusCodes.Status422UnprocessableEntity;
+                        problemDetails.Title = "One or more validation errors occured";
+
+                        return new UnprocessableEntityObjectResult(problemDetails)
+                        {
+                            ContentTypes = { "application/problem+json" }
+                        };
+                    };
+
+                    //If one or more problems not found/parsed, validate null/unparsable input
+                    problemDetails.Status = StatusCodes.Status400BadRequest;
+                    problemDetails.Title = "One or more input errors occured";
+                    return new BadRequestObjectResult(problemDetails)
+                    {
+                        ContentTypes = { "application/problem+json" }
+                    };
+                };
+            });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
