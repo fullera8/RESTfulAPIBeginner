9053ba5eb68e430372b49fcf05d8b3a249438c10
Alex Fuller <fulleralex47@gmail.com>
2021-03-12 07:31:35 -0500

    Add business rule validation to CoursesForCreationDto

--- Personal/CourseLibrary/CourseLibrary.API/Models/CoursesForCreationDto.cs
+++ Personal/CourseLibrary/CourseLibrary.API/Models/CoursesForCreationDto.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CourseLibrary.API.Models
 {
-    public class CoursesForCreationDto
+    public class CoursesForCreationDto : IValidatableObject
     {
         [Required]
         [MaxLength(100)]
@@ -15,5 +15,20 @@ namespace CourseLibrary.API.Models
 
         [MaxLength(1500)]
         public string Description { get; set; }
+
+        /// <summary>
+        /// Confirm business rule title does not match course
+        /// </summary>
+        /// <param name="validationContext">Provides access to course info we are working on</param>
+        /// <returns>Business rule validation</returns>
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                    "Description must be different from title.",
+                    new[] { "CoursesForCreationDto" });
+            }
+        }
     }
 }
